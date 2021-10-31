c3692f48ef46ce37cd7a960198b806894c4c589a
debasish ghosh <profile.dghosh@gmail.com>
2021-10-31 00:09:42 -0400

    all basic ejs functionality and starting point added

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use('/css',express.static(__dirname + 'public/css'));
 
 app.use(expressEjsLayouts);
+//EJS looks to 3 things - layout/css/js - layout is the starter file/css is the directory of css files/js is the directory for js files
 app.set('view engine', 'ejs');
-app.set()
+app.set('layout', './layouts/main')
+app.set('css',express.static(__dirname + 'public/css'));
+app.set('js',express.static(__dirname + 'public/js')); 
 
 app.get('/', (req,res)=>{ 
     res.render('index.ejs');
